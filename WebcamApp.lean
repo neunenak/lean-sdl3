23bead30bbc92cfb50f599598a1275697f0297dd
Greg Shuflin <greg@everydayimshuflin.com>
2026-07-08 22:21:03 -0700

    Remove unused code from webcam demo

--- WebcamApp.lean
+++ WebcamApp.lean
@@ -1,77 +1,9 @@
 import SDL
 
-structure Color where
-  r : UInt8
-  g : UInt8
-  b : UInt8
-  a : UInt8 := 255
-
-structure EngineState where
-  window : SDL.SDLWindow
-  renderer : SDL.SDLRenderer
-  deltaTime : Float
-  lastTime : UInt64
-  running : Bool
-  playerX : Int32
-  playerY : Int32
-  texture : SDL.SDLTexture
-  font : SDL.SDLFont
-  mixer : SDL.SDLMixer
-  track : SDL.SDLTrack
-  audio : SDL.SDLAudio
-
 def SCREEN_WIDTH : Int32 := 1280
 def SCREEN_HEIGHT : Int32 := 720
-def TEXTURE_SIZE : Float := 64.0
 
-inductive Key where
-  | W | A | S | D | Left | Right | Space | Escape
-
-def keyToScancode : Key → UInt32
-  | .W => SDL.SDL_SCANCODE_W | .A => SDL.SDL_SCANCODE_A | .S => SDL.SDL_SCANCODE_S
-  | .D => SDL.SDL_SCANCODE_D | .Left => SDL.SDL_SCANCODE_LEFT | .Right => SDL.SDL_SCANCODE_RIGHT
-  | .Space => SDL.SDL_SCANCODE_SPACE | .Escape => SDL.SDL_SCANCODE_ESCAPE
-
-def isKeyDown (key : Key) : IO Bool := SDL.getKeyState (keyToScancode key)
-
-def setColor (renderer : SDL.SDLRenderer) (color : Color) : IO Unit :=
-  SDL.setRenderDrawColor renderer color.r color.g color.b color.a *> pure ()
-
-def fillRect (renderer : SDL.SDLRenderer) (x y w h : Int32) : IO Unit :=
-  SDL.renderFillRect renderer { x, y, w, h } *> pure ()
-
-def renderScene (state : EngineState) : IO Unit := do
-  setColor state.renderer { r := 0, g := 0, b := 235 }
-  let _ ← SDL.renderClear state.renderer
-
-  setColor state.renderer { r := 255, g := 0, b := 0 }
-  fillRect state.renderer state.playerX state.playerY 100 100
-
-  let _ ← SDL.renderEntireTexture state.renderer state.texture 500 150 64 64
-
-  let _ ← SDL.renderTexture state.renderer state.texture 0 0 10 30 100 150 64 100
-
-  let message := "Hello, Lean SDL!"
-  let textSurface ← SDL.textToSurface state.renderer state.font message 50 50 255 255 255 255
-  let textTexture ← SDL.createTextureFromSurface state.renderer textSurface
-  let textWidth ← SDL.getTextureWidth textTexture
-  let textHeight ← SDL.getTextureHeight textTexture
-  let _ ← SDL.renderEntireTexture state.renderer textTexture 50 50 textWidth textHeight
-  pure ()
-
-private def updateEngineState (engineState : IO.Ref EngineState) : IO Unit := do
-  let state ← engineState.get
-  let currentTime ← SDL.getTicks
-  let deltaTime := (currentTime - state.lastTime).toFloat / 1000.0
-
-  let mut playerX := state.playerX
-  let mut playerY := state.playerY
-  if ← isKeyDown .A then playerX := playerX - 1
-  if ← isKeyDown .D then playerX := playerX + 1
-  if ← isKeyDown .W then playerY := playerY - 1
-  if ← isKeyDown .S then playerY := playerY + 1
-  engineState.set { state with deltaTime, lastTime := currentTime, playerX, playerY }
-
+def isEscapeKeyDown : IO Bool := SDL.getKeyState SDL.SDL_SCANCODE_ESCAPE
 
 structure WebcamState where
   window : SDL.SDLWindow
@@ -85,7 +17,7 @@ partial def webcamLoop (stateRef: IO.Ref WebcamState): IO Unit := do
 
     -- Check for quit event
     let eventType ← SDL.pollEvent
-    if eventType == SDL.SDL_QUIT || (← isKeyDown .Escape) then
+    if eventType == SDL.SDL_QUIT || (← isEscapeKeyDown) then
       stateRef.modify fun s => { s with running := false }
       return
 
@@ -130,28 +62,6 @@ partial def webcamLoop (stateRef: IO.Ref WebcamState): IO Unit := do
     let state ← stateRef.get
     if state.running then
       webcamLoop stateRef
-
-partial def gameLoop (engineState : IO.Ref EngineState) : IO Unit := do
-  updateEngineState engineState
-
-  let eventType ← SDL.pollEvent
-  if eventType == SDL.SDL_QUIT || (← isKeyDown .Escape) then
-    engineState.modify (fun s => { s with running := false })
-
-  if eventType == SDL.SDL_MOUSEBUTTONDOWN then
-    let (mouseX, mouseY) ← SDL.getMousePos
-    if ← SDL.isLeftMousePressed then
-      IO.println s!"Left click at ({mouseX}, {mouseY})"
-    if ← SDL.isRightMousePressed then
-      IO.println s!"Right click at ({mouseX}, {mouseY})"
-    if ← SDL.isMiddleMousePressed then
-      IO.println s!"Middle click at ({mouseX}, {mouseY})"
-
-  let state ← engineState.get
-  if state.running then
-    renderScene state
-    SDL.renderPresent state.renderer
-    gameLoop engineState
 
 partial def run : IO Unit := do
   unless (← SDL.init (SDL.SDL_INIT_VIDEO ||| SDL.SDL_INIT_CAMERA)) == 1 do
